2a71fe42558dd988dddfba531da8aa92fde5757b
andersonsaleslima <a.sales.lima97@gmail.com>
2019-02-24 15:26:53 -0300

    Update install-dependecies.sh

--- install-dependecies.sh
+++ install-dependecies.sh
@@ -1,9 +1,7 @@
-#!/bin/sh
-
 echo "Instaladno TCPDump"
 pkg install -y tcpdump
 
-if (( $? != 0 ))
+if [ $? -ne 0 ]
 then
         echo "Pacote Tcpdump não instalad"
         exit
@@ -12,9 +10,8 @@ fi
 echo "Instaladno tmux"
 pkg install -y tmux
 
-if (( $? != 0 ))
+if [ $? -ne  0 ]
 then
         echo "Pacote tmux não instaldo"
         exit
 fi
-
